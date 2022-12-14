9a88584c7d236f9f017f2e7beaf210a41d9bda22
Bence Komarniczky <niczky12@gmail.com>
2022-12-14 12:15:53 +0000

    add checks

--- branch-name-hook/branchcheck.py
+++
@@ -1,8 +0,0 @@
-import subprocess
- 
-# Using system() method to
-# execute shell commands
-process = subprocess.Popen('git rev-parse --abbrev-ref HEAD', shell=True, stdout=subprocess.PIPE)
-local_branch = process.stdout.read().decode('utf-8').strip()
-
-print(local_branch)
