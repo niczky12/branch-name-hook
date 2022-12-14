19eef267bb33becf71476fadcca88c4a741c6707
Bence Komarniczky <niczky12@gmail.com>
2022-12-14 12:24:09 +0000

    allow main and master

--- run-test-hook.sh
+++ run-test-hook.sh
@@ -6,7 +6,7 @@ echo "$@"
 
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
-valid_release_branch="^main|master"
+valid_release_branch="^(main)|(master)$"
 valid_dev_branch="^(?:[a-z]{3})-([A-Z0-9]+\-\d+)-([a-zA-Z0-9\-]+)$"
 
 message="There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your commit will be rejected. You should rename your branch to a valid name and try again."
